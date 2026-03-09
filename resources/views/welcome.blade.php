bd1dc23cf84aac50d11609aa2ea0b51ae1c6d1e9
edithsotoramirez <167482570+edithsotoramirez@users.noreply.github.com>
2026-03-08 22:34:56 -0700

    aggregué un heading con mi nombre en welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,6 +55,8 @@
                     <h1 class="mb-1 font-medium">Let's get started</h1>
                     <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">Laravel has an incredibly rich ecosystem. <br>We suggest starting with the following.</p>
                     <ul class="flex flex-col mb-4 lg:mb-6">
+                        <h1>edith soto</h1>
+                    
                         <li class="flex items-center gap-4 py-2 relative before:border-l before:border-[#e3e3e0] dark:before:border-[#3E3E3A] before:top-1/2 before:bottom-0 before:left-[0.4rem] before:absolute">
                             <span class="relative py-1 bg-white dark:bg-[#161615]">
                                 <span class="flex items-center justify-center rounded-full bg-[#FDFDFC] dark:bg-[#161615] shadow-[0px_0px_1px_0px_rgba(0,0,0,0.03),0px_1px_2px_0px_rgba(0,0,0,0.06)] w-3.5 h-3.5 border dark:border-[#3E3E3A] border-[#e3e3e0]">
